a15de49aac9af7419b88e1f370dff9fb48270978
Collin-W <whalencollin@gmail.com>
2022-01-17 10:49:02 -0600

    thought routes and thought controllers modified

--- controllers/Thought.js
+++ controllers/Thought.js
@@ -1,55 +1,44 @@
 // import Thought from '../models'
 // import User from '../models'
 
-const { Thought, User } = require('../models');
+const { Thought, User } = require("../models");
 
 const thoughtController = {
+  // getAllThought(req, res) {
 
-    
-    // getAllThought(req, res) {
-     
-    //       .then(dbUsersData => res.json(dbUsersData))
-    //       .catch(err => {
-    //         console.log(err);
-    //         res.status(400).json(err);
-    //       });
-    //   },
+  //       .then(dbUsersData => res.json(dbUsersData))
+  //       .catch(err => {
+  //         console.log(err);
+  //         res.status(400).json(err);
+  //       });
+  //   },
 
+  // getThoughtById,
 
+  createThought({ params, body }, res) {
+    console.log(params);
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        console.log(dbUserData);
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 
+  // updateThought,
 
-    // getThoughtById
+  //addReaction,
+};
 
-
-
-    
-    createThought({ params, body }, res) {
-        console.log(params);
-        Thought.create(body)
-          .then(({ _id }) => {
-            return User.findOneAndUpdate(
-              { _id: params.userId },
-              { $push: { thoughts: _id } },
-              { new: true }
-            );
-          })
-          .then(dbUserData => {
-            console.log(dbUserData);
-            if (!dbUserData) {
-              res.status(404).json({ message: 'No User found with this id!' });
-              return;
-            }
-            res.json(dbUserData);
-          })
-          .catch(err => res.json(err));
-      },
-
-
-
-
-    // updateThought,
-    
-
-}
-
-module.exports = thoughtController
+module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,18 +2,26 @@ const router = require('express').Router();
 
 const {
     createThought,
+    getAllThought,
+    getThoughtById,
+    addReaction,
+    updateThought
 } = require('../../controllers/Thought')
 
 
 router
   .route('/')
-//   .get(getAllThought)
+  .get(getAllThought)
   .post(createThought);
 
-// router
-//   .route('/:id')
-//   .get(getThoughtById)
-//   .put(updateThought)
-//   .delete(deleteThought);
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router
+.route('/:thoughtId/reaction')
+.post(addReaction)
 
 module.exports = router;
